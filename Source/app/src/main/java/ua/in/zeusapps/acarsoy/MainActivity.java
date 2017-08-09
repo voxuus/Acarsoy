39f986fa647ac24337e7ed66581240429738d449
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-08-09 16:29:06 +0300

    added stub for showing login page

--- Source/app/src/main/java/ua/in/zeusapps/acarsoy/MainActivity.java
+++ Source/app/src/main/java/ua/in/zeusapps/acarsoy/MainActivity.java
@@ -1,13 +1,25 @@
 package ua.in.zeusapps.acarsoy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ua.in.zeusapps.acarsoy.activities.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.activity_main_login_screen)
+    public void showLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+
+        startActivity(intent);
     }
 }

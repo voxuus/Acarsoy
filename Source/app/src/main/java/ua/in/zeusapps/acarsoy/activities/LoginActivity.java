fae737bbbec993c405775e0f2386e3a4c4b3b0a6
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-08-09 15:02:36 +0300

    added login activity

---
+++ Source/app/src/main/java/ua/in/zeusapps/acarsoy/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package ua.in.zeusapps.acarsoy.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ua.in.zeusapps.acarsoy.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

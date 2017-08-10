c05a787156f69f3fed4089d12cf31c6de447f282
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-08-10 15:10:33 +0300

    added view for plant productivity

--- Source/app/src/main/java/ua/in/zeusapps/acarsoy/activities/PlantDetailsActivity.java
+++ Source/app/src/main/java/ua/in/zeusapps/acarsoy/activities/PlantDetailsActivity.java
@@ -3,22 +3,29 @@ package ua.in.zeusapps.acarsoy.activities;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import ua.in.zeusapps.acarsoy.R;
 import ua.in.zeusapps.acarsoy.models.Plant;
+import ua.in.zeusapps.acarsoy.models.PlantProductivity;
 import ua.in.zeusapps.acarsoy.services.FakePlantService;
 import ua.in.zeusapps.acarsoy.services.IPlantService;
 
 public class PlantDetailsActivity extends AppCompatActivity {
 
     private final IPlantService _plantService = new FakePlantService();
+    private final DecimalFormat _df = new DecimalFormat("0.0");
 
     @BindView(R.id.activity_plant_details_image_holder)
     FrameLayout _imageHolder;
@@ -28,6 +35,8 @@ public class PlantDetailsActivity extends AppCompatActivity {
     TextView _temperatureTextView;
     @BindView(R.id.activity_plant_details_name)
     TextView _nameTextView;
+    @BindView(R.id.activity_plant_details_recyclerView)
+    RecyclerView _recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +46,11 @@ public class PlantDetailsActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Plant plant = _plantService.getPlant();
-        showData(plant);
+        List<PlantProductivity> productivities = _plantService.getPlantProductivity(plant);
+        showData(plant, productivities);
     }
 
-    private void showData(Plant plant) {
+    private void showData(Plant plant, List<PlantProductivity> productivities) {
         int imageRes = 0;
         int backgroundRes = 0;
         switch (plant.getType()){
@@ -57,9 +67,67 @@ public class PlantDetailsActivity extends AppCompatActivity {
         int color = ContextCompat.getColor(this, backgroundRes);
         _imageHolder.setBackgroundColor(color);
 
-        DecimalFormat df = new DecimalFormat("#.0");
-        String temperature = df.format(plant.getTemperature()) + "°C";
+        String temperature = String.format(
+                getString(R.string.temperature),
+                _df.format(plant.getTemperature()));
         _temperatureTextView.setText(temperature);
         _nameTextView.setText(plant.getName());
+
+        Adapter adapter = new Adapter(productivities);
+        _recyclerView.setAdapter(adapter);
+        _recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    class Holder extends RecyclerView.ViewHolder {
+        @BindView(R.id.template_plant_productivity_details_power)
+        TextView powerTextView;
+        @BindView(R.id.template_plant_productivity_details_wind)
+        TextView windTextView;
+
+        public Holder(View itemView) {
+            super(itemView);
+
+            ButterKnife.bind(this, itemView);
+        }
+
+        void update(PlantProductivity productivity) {
+
+
+
+            powerTextView.setText(
+                    String.format(
+                            getString(R.string.mega_watts),
+                            _df.format(productivity.getPower())));
+            windTextView.setText(
+                    String.format(
+                            getString(R.string.meters_per_second),
+                            _df.format(productivity.getWind())));
+        }
+    }
+
+    private class Adapter extends RecyclerView.Adapter<Holder> {
+
+        private final List<PlantProductivity> _items;
+
+        public Adapter(List<PlantProductivity> items) {
+            _items = items;
+        }
+
+        @Override
+        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = getLayoutInflater()
+                    .inflate(R.layout.template_plant_productivity_details, parent, false);
+            return new Holder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int position) {
+            holder.update(_items.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return _items.size();
+        }
     }
 }
